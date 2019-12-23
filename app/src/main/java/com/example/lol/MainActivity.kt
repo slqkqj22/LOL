75e8cd9a63c1041137f6ada13c393829a91028ac
slqkqj22 <qkrtjrgus11@gmail.com>
2019-12-23 12:43:13 +0900

    test

--- app/src/main/java/com/example/lol/MainActivity.kt
+++ app/src/main/java/com/example/lol/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.lol
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val constss = Constss("HELLO WORLD!!")
+        //binding.setVariable(BR.constss, constss)
+        binding.executePendingBindings()
+
     }
 }
+
+data class Constss(val hello : String)
